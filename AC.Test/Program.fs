f904fed78452cbe860b2bc3824bbfb0a9b7c98bd
ttak0422 <bgdaewalkman@gmail.com>
2020-11-19 18:02:06 +0900

    first commit

---
+++ AC.Test/Program.fs
@@ -0,0 +1,20 @@
+﻿module AC.Test
+
+open System
+open Expecto
+open Expecto
+open AC 
+
+[<Tests>]
+let libraryTests = 
+    testList "AC.Library" [
+        test "add" {
+            Expect.equal (Library.add 1 2) 3 "1+2"
+        }
+        testProperty "交換法則" <| fun a b -> 
+            Library.add a b = Library.add b a
+    ]
+
+[<EntryPoint>]
+let main argv =
+    runTestsInAssembly defaultConfig argv
--- shell.nix
+++ shell.nix
@@ -1,8 +1,13 @@
 let 
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs = import sources.nixpkgs {
+    overlays = [ 
+      (import ./nix/dotnet-sdk-overlay.nix) 
+    ]; 
+  };
 in pkgs.mkShell {
   buildInputs = with pkgs; [
+    dotnet-sdk
     # keep this line if you use bash
     bashInteractive
   ];
