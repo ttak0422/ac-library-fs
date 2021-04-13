6a77c65767165515bc0d7c7da167e2c84f326d58
ttak0422 <bgdaewalkman@gmail.com>
2021-04-13 22:15:07 +0900

    update nix configs
    update niv
    update nixpkgs branch
    add .NET5 for ionide

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
-let 
+let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {
-    overlays = [ 
-      (import ./nix/dotnet-sdk-overlay.nix) 
-    ]; 
+    overlays = [ (import ./nix/dotnet-sdk-overlay.nix) ];
   };
+  dotnetCombined = with pkgs.dotnetCorePackages;
+    combinePackages [ pkgs.dotnet-sdk pkgs.dotnet-sdk_5 ];
 in pkgs.mkShell {
   buildInputs = with pkgs; [
-    dotnet-sdk
+    dotnetCombined
     # keep this line if you use bash
     bashInteractive
   ];
